eeabd4388d9295ffa0ee678d94e3c57509d71959
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-08-07 00:21:26 +0530

    Update mattrix.py

--- pybasics/matrix.py
+++ pybasics/matrix.py
@@ -1,25 +1,26 @@
+# Write a python program for demonstrating use of numPy
 import numpy as n
 a=n.array([[10,15]])
 b=n.array([[15,20]])
-print(a)
-print(b)
-print('add: ',a+b)
-print('transpose: ',a.T+b.T)
-print('sub: ',a-b)
+print('\nA: ',a)
+print('\nB:',b)
+print('\nadd: ',a+b)
+print('\ntranspose: ',a.T+b.T)
+print('\nsub: ',a-b)
 #scalar
 s=5
 c=n.array([[5,7]])
-print('scalar mul: ',s*c)
+print('\nscalar mul: ',s*c)
 #matrix mul
-print('matrix mul: ',a*b)
+print('\nmatrix mul: ',a*b)
 #dot mul
-print('dot mul: ',n.dot(a,b.T))
+print('\ndot mul: ',n.dot(a,b.T))
 #matrix sum
-print('matrix sum: ',a+b)
+print('\nmatrix sum: ',a+b)
 #identity
 im=n.eye(4)
-print('identity matrix: ',im)
+print('\nidentity matrix: ',im)
 #inverse
 d=([[1,2,0],[0,1,2],[2,0,1]])
-print('D: ',d)
-print('inverse: ',n.linalg.inv(d))
+print('\nD: ',d)
+print('\ninverse: ',n.linalg.inv(d))
