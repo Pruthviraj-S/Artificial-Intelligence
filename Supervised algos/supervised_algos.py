acbea6a21e54a6a52750bf5a6b24d6f99860e91e
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-08-07 20:21:20 +0530

    Update SVM

--- Supervised algos/supervised_algos.py
+++ Supervised algos/supervised_algos.py
@@ -15,19 +15,20 @@ from sklearn.svm import SVC
 dataset = pd.read_csv('../iris dataset/iris.csv')
 
 # Shape dataset
-print('\n(Rows,Columns): ',dataset.shape)
+print('\n(Rows,Columns): ', dataset.shape)
 
 # First 10 Entries
-print('\nFirst 10 Entries:\n',dataset.head(10))
+print('\nFirst 10 Entries:\n', dataset.head(10))
 
 # Describe dataset
-print('\nDescribe dataset:\n',dataset.describe())
+print('\nDescribe dataset:\n', dataset.describe())
 
 # group data and check for size
-print('\nGroup:',dataset.groupby('species').size())
+print('\nGroup:', dataset.groupby('species').size())
 
 # box plot
-dataset.plot(kind='box',subplots=True,layout=(2,2),sharex=False,sharey=False)
+dataset.plot(kind='box', subplots=True, layout=(
+    2, 2), sharex=False, sharey=False)
 plt.show()
 
 # plot histogram
@@ -39,42 +40,44 @@ scatter_matrix(dataset)
 plt.show()
 
 # Heatmap
-heatplot=sb.heatmap(dataset.corr(),cmap='YlGnBu',annot=True)
+heatplot = sb.heatmap(dataset.corr(), cmap='YlGnBu', annot=True)
 plt.show()
 
 # split data, train & test
 array = dataset.values
-X = array[:,0:4]
-Y = array[:,4]
-validation_size=0.20
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20
 seed = 6
 scoring = 'accuracy'
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=validation_size, random_state = seed)
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
+    X, Y, test_size=validation_size, random_state=seed)
 
 # models
 models = []
-models.append (('LR', LogisticRegression()))
-models.append (('LDA', LinearDiscriminantAnalysis()))
-models.append (('KNN', KNeighborsClassifier()))
-models.append (('CART', DecisionTreeClassifier()))
-models.append (('NB', GaussianNB()))
-models.append (('SVM', SVC()))
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
 
 results = []
 names = []
 
 # crosss validation, data is split into parts of 10.
 for name, model in models:
-    kfold = model_selection.KFold(n_splits = 10)
-    cv_results=model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring= 'accuracy')
+    kfold = model_selection.KFold(n_splits=10)
+    cv_results = model_selection.cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring='accuracy')
     results.append(cv_results)
     names.append(name)
     print("%s:  %f  (%f)" % (name, cv_results.mean(), cv_results.std()))
 
 # comparision of algos
-fig =plt.figure()
+fig = plt.figure()
 fig.suptitle('Algorithm Comparision')
-ax= fig.add_subplot(111)
+ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax. set_xticklabels(names)
 plt.show()
