871e3f925eb3b425701bcee5cf9f7a0d91584725
PruthvirajS <62303937+Pruthviraj-S@users.noreply.github.com>
2022-08-07 20:21:50 +0530

    Create KNN

---
+++ KNN/knn.py
@@ -0,0 +1,35 @@
+# KNN
+import pandas
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import model_selection
+
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv('../iris dataset/iris.csv', names=names)
+
+print('Head: \n', dataset.head(5))
+print('Describe: \n', dataset.describe())
+print(dataset.groupby('class').size())
+
+# array
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20
+seed = 6
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(
+    X, Y, test_size=validation_size, random_state=seed)
+
+seed = 6
+scoring = 'accuracy'
+
+# model ; knn
+models = []
+models.append(('KNN', KNeighborsClassifier()))
+
+results = []
+kfold = model_selection.KFold(n_splits=10)
+cv_results = model_selection.cross_val_score(
+    KNeighborsClassifier(), X_train, Y_train, cv=kfold, scoring=scoring)
+results.append(cv_results)
+value = "%f" % (cv_results.mean())
+print('\nValue: ', value)
--- decision treee/decision_tree.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn import tree
-
-# import dataset
